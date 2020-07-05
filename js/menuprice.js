fce01243013ba3d5cbe3e295eef01c1f7e435304
Cyrus Liang <thecyrusliang@gmail.com>
2020-07-04 23:36:38 -0700

    Update menuprice

--- js/menuprice.js
+++ js/menuprice.js
@@ -3,5 +3,7 @@
  */
 $(document).ready(function(){
     document.getElementById("gyoza").innerHTML = "$9.50";
-    document.getElementById("eggplant").innerHTML = "$11";
+    document.getElementById("eggp").innerHTML = "$11";
+    document.getElementById("misoyaki").innerHTML = "$17";
+    document.getElementById("bnegi").innerHTML = "$16";
 })
