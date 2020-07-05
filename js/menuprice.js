108d6bc54e8f37e3836d005e5a58d34a53ae02c9
Cyrus Liang <thecyrusliang@gmail.com>
2020-07-05 00:22:39 -0700

    Update menuprice.js

--- js/menuprice.js
+++ js/menuprice.js
@@ -6,4 +6,9 @@ $(document).ready(function(){
     document.getElementById("eggp").innerHTML = "$11";
     document.getElementById("misoyaki").innerHTML = "$17";
     document.getElementById("bnegi").innerHTML = "$16";
+    document.getElementById("hkama").innerHTML = "$17";
+    document.getElementById("ckushi").innerHTML = "$13.50";
+    document.getElementById("karaage").innerHTML = "$10";
+    document.getElementById("svtempura").innerHTML = "$14";
+    document.getElementById("stempura").innerHTML = "$12.50";
 })
