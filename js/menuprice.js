6bd5408c1dc0dfad924de73fdd63d79beb12e016
Cyrus Liang <thecyrusliang@gmail.com>
2020-07-04 22:58:59 -0700

    Create menuprice.js

---
+++ js/menuprice.js
@@ -0,0 +1,7 @@
+/*!
+ * Displays menu prices
+ */
+$(document).ready(function(){
+    document.getElementById("gyoza").innerHTML = "$9.50";
+    document.getElementById("eggplant").innerHTML = "$11";
+})
