cc59e86a4950752c9728602ffa96b1a85de73f4d
Cyrus Liang <thecyrusliang@gmail.com>
2020-07-20 15:35:38 -0700

    Update appetizers.html

--- js/menuprice.js
+++ js/menuprice.js
@@ -1,14 +1,26 @@
 /*!
  * Displays menu prices
+    document.getElementById("").innerHTML = "$";
  */
 $(document).ready(function(){
-    document.getElementById("gyoza").innerHTML = "$9.50";
-    document.getElementById("eggp").innerHTML = "$11";
-    document.getElementById("misoyaki").innerHTML = "$17";
-    document.getElementById("bnegi").innerHTML = "$16";
+    // Appetizers
+    document.getElementById("gyoza").innerHTML = "$8.50";
+    document.getElementById("eggp").innerHTML = "$9.50";
+    document.getElementById("misoyaki").innerHTML = "$16";
+    document.getElementById("bnegi").innerHTML = "$14.50";
     document.getElementById("hkama").innerHTML = "$17";
-    document.getElementById("ckushi").innerHTML = "$13.50";
-    document.getElementById("karaage").innerHTML = "$10";
-    document.getElementById("svtempura").innerHTML = "$14";
-    document.getElementById("stempura").innerHTML = "$12.50";
+    document.getElementById("ckushi").innerHTML = "$12";
+    document.getElementById("karaage").innerHTML = "$9";
+    document.getElementById("svtempura").innerHTML = "$12.50";
+    document.getElementById("stempura").innerHTML = "$11";
+    document.getElementById("vtempura").innerHTML = "$10";
+    document.getElementById("sscrab").innerHTML = "$12.50";
+    document.getElementById("korokke").innerHTML = "$8.50";
+    document.getElementById("kakifry").innerHTML = "$10.50";
+    document.getElementById("atofu").innerHTML = "$9.50";
+    document.getElementById("ktofu").innerHTML = "$10";
+    document.getElementById("htofu").innerHTML = "$9";
+
+    // Salads
+    
 })
